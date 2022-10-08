c7c8953ef4366a66f6c3c9d08da31157957fa251
Maksym <divine.anti.mage@gmail.com>
2022-10-08 11:54:33 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.request__carousel').slick(
+		{
+			speed: 1400,
+			autoplay: false,
+			autoplaySpeed: 2500,
+			prevArrow: '<button type="button" class="slick-prev"><div></div></button>',
+			nextArrow: '<button type="button" class="slick-next"><div></div></button>'
+		});
+});
