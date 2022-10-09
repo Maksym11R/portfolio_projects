d95479a89903339517f110aa9f68120823f1456a
Maksym <divine.anti.mage@gmail.com>
2022-10-09 21:47:53 +0300

    slider2

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 			speed: 1400,
 			autoplay: false,
 			autoplaySpeed: 2500,
-			prevArrow: '<button type="button" class="slick-prev"><div></div></button>',
-			nextArrow: '<button type="button" class="slick-next"><div></div></button>'
+			prevArrow: '<button type="button" class="slick-prev"><img src="icons/icons_slider/lefts.svg"></button>',
+			nextArrow: '<button type="button" class="slick-next"><img src="icons/icons_slider/rights.svg"></button>'
 		});
 });
